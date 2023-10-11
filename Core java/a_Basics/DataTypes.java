f18ace0f5e554c2d12bfc0812c65effa6103b104
Rishav Kumar <rishav2freelance@gmail.com>
2023-10-11 15:42:35 +0530

    Changed notes.
    Detailed explained notes.

---
+++ Core java/a_Basics/DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes{
+    public static void main(String args[]){
+
+
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1+num2;
+        System.out.println(result);
+
+        // System.out.println(3+5);
+        // System.out.println(8+7);
+
+        // System.out.println("Hello, World!");
+    }
+}
